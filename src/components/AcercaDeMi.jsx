b3556c4afed568f0583fe88a37aac66322f6f203
PedroArias1800 <pedrosoftdev18@gmail.com>
2023-10-18 23:49:33 -0500

    Quitar botones de Conoceme y colocar info en el Footer

--- src/components/AcercaDeMi.jsx
+++ src/components/AcercaDeMi.jsx
@@ -6,13 +6,10 @@ export function AcercaDeMi() {
     <div className="acercaDeMi" id="acercaDeMi">
         <h2><br />Conóceme</h2>
         <div>
-            <p>Daniel Lombana es un joven abogado, docente, artista y político motivado por su vocación de servicio a la mejora de la institucionalidad en el país y en romper el status quo prestablecido.
-                Tiene una maestría en Derecho Procesal y una Licenciatura en Derecho de la USMA, una especialidad en Docencia Superior por la ULACIT, y múltiples estudios en Derecho Constitucional y Procesal Constitucional, Derecho Administrativo, Seguridad Social, Discapacidad, Liderazgo Legislativo, entre otros.
-                </p>
-            <div className="divBotones">
-                <button className="hire-button">Hire Me</button>
-                <button className="hire-button hire-button2">View CV</button>
-            </div>
+            <p>
+              Daniel Lombana es un joven abogado, docente, artista y político motivado por su vocación de servicio a la mejora de la institucionalidad en el país y en romper el status quo prestablecido.
+              Tiene una maestría en Derecho Procesal y una Licenciatura en Derecho de la USMA, una especialidad en Docencia Superior por la ULACIT, y múltiples estudios en Derecho Constitucional y Procesal Constitucional, Derecho Administrativo, Seguridad Social, Discapacidad, Liderazgo Legislativo, entre otros.
+            </p>
         </div>
     </div>
   )
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export function Footer() {
   return (
-    <footer style={{"backgroundColor": "#1C2545", "color": "#F8F9FA", "textAlign": "center", "padding": "20px"}}>
-    
+    <footer style={{"backgroundColor": "#1C2545", "color": "#F8F9FA", "textAlign": "center", "padding": "20px", "fontSize": "1.3rem"}}>
+      <p>&copy; <span id="copyright-year">2023</span> Daniel Lombana, Candidato Independiente a Diputado. Todos los derechos reservados.</p>
     </footer>
   )
 }
