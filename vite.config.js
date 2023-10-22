725f7fdb61656313a3d9856c91bc62b78d8d13ed
LuisCeballos24 <lfceballos@playdigitalconsulting.com>
2023-10-22 12:07:52 -0500

    hola
    hola

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,26 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  server: {
-    port: 3000, // Puedes ajustar el puerto según sea necesario
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: '/src/main.jsx', // Ajusta la ruta de tu punto de entrada según sea necesario
+      output: {
+        manualChunks: {
+          // Agrupa archivos .jsx en un chunk separado
+          jsx: ['/src/**/*.jsx'],
+
+          // Agrupa archivos .css en un chunk separado
+          css: ['/src/**/*.css'],
+
+          // Agrupa archivos .json en un chunk separado
+          json: ['/src/**/*.json'],
+
+          // Agrupa todos los demás archivos en el chunk principal
+          main: ['/src/**/*.{js,jsx,ts,tsx,css,json}'],
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Ajusta este límite según sea necesario
   },
 });
