0901849a7b39923a7c8fffa717b8da218417bffc
PedroArias1800 <pedrosoftdev18@gmail.com>
2023-10-22 12:18:57 -0500

    Si

--- vite.config.js
+++ vite.config.js
@@ -2,26 +2,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: '/src/main.jsx', // Ajusta la ruta de tu punto de entrada según sea necesario
-      output: {
-        manualChunks: {
-          // Agrupa archivos .jsx en un chunk separado
-          jsx: ['/src/**/*.jsx'],
-
-          // Agrupa archivos .css en un chunk separado
-          css: ['/src/**/*.css'],
-
-          // Agrupa archivos .json en un chunk separado
-          json: ['/src/**/*.json'],
-
-          // Agrupa todos los demás archivos en el chunk principal
-          main: ['/src/**/*.{js,jsx,ts,tsx,css,json}'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // Ajusta este límite según sea necesario
+  server: {
+    port: 5173
   },
 });
